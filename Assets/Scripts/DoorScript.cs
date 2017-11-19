d6388f13a3ed4c1a5df1ea1c6ccad6355e597f71
Guillaume <guillaume.nougier@ynov.com>
2017-11-19 12:53:21 +0100

    init commit

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+
+    private bool canOpen = false;
+
+    void OnTriggerEnter(Collider c)
+    {
+        canOpen = true;
+    }
+
+  
+
+    void Update()
+    {
+        if (canOpen)
+        {
+            Up();
+        }     
+    }
+
+    void Up()
+    {
+        Vector3 pos = this.transform.position;
+        this.transform.localPosition += this.transform.up * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+    [SerializeField]
+    private float Speed;
+
+	// Use this for initialization
+	void Start () {
+        //this.transform.localScale = new Vector3(10, 10, 10);
+        this.transform.GetComponent<SphereCollider>().radius = 0.5f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float factor = Time.deltaTime * Speed;
+        this.transform.localScale += new Vector3(factor, factor, factor);
+    }
+}
